5e72cce804b63403f4cd87c4e1f0ada75188973b
EDEM-PC\Edem <ehdemdume@gmail.com>
2017-02-27 11:43:35 -0500

    web page

---
+++ Practice/controller.js
@@ -0,0 +1,19 @@
+
+angular.module('myApp', [])
+    .controller('myCtrl', ['$scope', '$http', function($scope, $http) {
+        $scope.myFunc = function(){
+            var auth = window.btoa($scope.userName + ':' + $scope.lastName), headers = {"Authorization": "Basic " + auth};
+            $http.get("http://34.200.170.137/abcblock/login", {headers: headers}).then(function (response) {
+                if (response.status === 200)
+                {
+                    $scope.ctObject = response.data;
+
+                    $scope.my_variable = $scope.ctObject.first_name + ' ' + $scope.ctObject.last_name;
+                }
+                else
+                {
+                    $scope.my_variable = "YOU FREAKING SUCK BRO";
+                }
+            });
+        }
+    }]);
