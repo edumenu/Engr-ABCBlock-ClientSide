f9846b9183bc06467c33564e3b6e829102e31135
EDEM-PC\Edem <ehdemdume@gmail.com>
2017-05-01 01:18:05 -0400

    Updated version

--- home/controller.js
+++ home/controller.js
@@ -1,23 +1,52 @@
+//Author: Edem Dumenu
+//Creation: 03-02-2017
+//Development Team: Edem Dumenu, Luke Clark, Sam Justice
+//Description: This activity facilitates the both the login page and the create user page
+//It receives the User's credentials and passes that information to the server for authentication
 
+
+//Name: New  module
+//Author: Edem Dumenu
+//Date: 03-05-2017
+//Purpose: This is creating a new module called app
+//Arguments: "myApp" which is the name of the app
+//Errors/Exceptions: None
+//Modification history: This is the original version
 var app = angular.module("myApp", []);
+
+//Name: New  module
+//Author: Edem Dumenu
+//Date: 03-05-2017
+//Purpose: This is creating a new controller to retrieve the User's first and last name to be sent to the server
+//for authentication
+//Arguments: "myCtrl" which is the name of the controller. $scope which is the scope object. $http which is an angularjs service
+//for reading data from server. $window which represents an open window in a browser
+//Errors/Exceptions: None
+//Modification history: This is the original version
 app.controller('myCtrl', ['$scope', '$http', function($scope, $http,$window) {
     $scope.myFunc = function(){
         var auth = window.btoa($scope.firstName + ':' + $scope.lastName), headers = {"Authorization": "Basic " + auth};
         $http.get("http://34.200.170.137/abcblock/login", {headers: headers}).then(function (response) {
             $scope.ctObject = response.data;
-            //$scope.message = $scope.ctObject.id;
-            var myUrl = window.location = "profilePage.html";
-            //$scope.variable = $scope.ctObject.id + ' ' + $scope.ctObject.last_name;
+            window.localStorage['ctID'] = angular.toJson($scope.ctObject.id);
+            var myUrl = window.location = "root/profileListPage.html";
             $window.open(myUrl);
-        }, function (response)
+        }, function ()
         {
             $scope.variable = "Invalid input!";
         });
     }
-}])
+}]);//end of myCtrl
 
-
-
+//Name: New  module
+//Author: Edem Dumenu
+//Date: 03-05-2017
+//Purpose: This is creating a new controller to retrieve the User's first and last name, user name and password
+// to be sent to the server to create a new user.
+//Arguments: "myCtrl1" which is the name of the controller. $scope which is the scope object. $http which is an angularjs service
+//for reading data from server. $window which represents an open window in a browser
+//Errors/Exceptions: None
+//Modification history: This is the original version
 app.controller('myCtrl1', ['$scope', '$http', function($scope, $http) {
     $scope.myFunc1 = function(){
         var ctObject = ({
@@ -27,7 +56,7 @@ app.controller('myCtrl1', ['$scope', '$http', function($scope, $http) {
             password: $scope.passWord
         });
         $http.post('http://34.200.170.137/abcblock/cts', ctObject)
-            .then(function sucessCallback (ctObject) {
+            .then(function successCallback (ctObject) {
                     ctObject.id = data.insertId;
                     $scope.ctObject = ctObject;
                 }
@@ -35,5 +64,5 @@ app.controller('myCtrl1', ['$scope', '$http', function($scope, $http) {
                     $scope.my_variable1 = "Invalid Input!";
                 });
     };
-}]);
+}]);//end of myCtrl
 
--- home/controller2.js
+++ home/controller2.js
@@ -1,75 +1,207 @@
+//Author: Edem Dumenu
+//Creation: 03-02-2017
+//Development Team: Edem Dumenu, Luke Clark, Sam Justice
+//Description: This activity facilitates the CT's profile page, the patient list page and the patient statistics page
+//It receives the User's credentials and passes that information to the server for authentication
+
+//Name: New  module
+//Author: Edem Dumenu
+//Date: 03-05-2017
+//Purpose: This is creating a new module called app
+//Arguments: "myApp" which is the name of the app
+//Errors/Exceptions: None
+//Modification history: This is the original version
 var app = angular.module('myApp2', []);
+
+//Name: New  module
+//Author: Edem Dumenu
+//Date: 03-07-2017
+//Purpose: This is creating a new controller to retrieve the name of the CT who's signed in
+//Arguments: "myCtrl2" which is the name of the controller. $scope which is the scope object. $http which is an angularjs service
+//for reading data from server.
+//Errors/Exceptions: None
+//Modification history: This is the original version
 app.controller('myCtrl2', ['$scope', '$http', function($scope, $http) {
-    $http.get("http://34.200.170.137/abcblock/cts/1", {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} }).then(function (response) {
+    var accessData = window.localStorage['ctID'];
+    $http.get("http://34.200.170.137/abcblock/cts/" + accessData,
+        {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} })
+        .then(function (response) {
         $scope.ctObject = response.data;
-        //$scope.message = "Success!!";
         $scope.message = $scope.ctObject.first_name + ' ' + $scope.ctObject.last_name;
     }, function (response)
     {
         $scope.message = "Invalid input!";
     });
+}]);//end of myCtrl2
+
+//Name: New  module
+//Author: Edem Dumenu
+//Date: 03-07-2017
+//Purpose: This is creating a new controller to retrieve the patient list associated with the CT
+//Arguments: "patientList" which is the name of the controller. $scope which is the scope object. $http which is an angularjs service
+//for reading data from server. $sce which provides strict contextual escaping services to angularjs
+//Errors/Exceptions: None
+//Modification history: This is the original version
+app.controller('patientList', ['$scope', '$http','$sce', function($scope, $http, $sce) {
+    var accessData = window.localStorage['ctID'];
+    $http.get("http://34.200.170.137/abcblock/cts/" + accessData + "/patients",
+        {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} })
+        .then(function (response) {
+        $scope.items = response.data;
+        $scope.message = "Success!!";
+    }, function (response)
+    {
+        $scope.message = "Invalid input!";
+    });
+
+    //Name: New  module
+//Author: Edem Dumenu
+//Date: 03-07-2017
+//Purpose: This function is retrieving a patient's ID number when you select a patient. Then it stores the patient's ID number in
+// a local storage called "patientNum". It then opens the "profilePage.html" in the iframe.
+//Arguments: "patientID" which is the patient's ID.
+//Errors/Exceptions: None
+//Modification history: This is the original version
+    $scope.removeID = function(patientID){
+        window.localStorage['patientNum'] = angular.toJson(patientID);
+        $scope.detailFrame = $sce.trustAsResourceUrl("profilePage.html");
+    }
+
 }]);
 
-app.controller('myCtrl3', ['$scope', '$http', function($scope, $http) {
-    $http.get("http://34.200.170.137/abcblock/cts/1/patients", {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} }).then(function (response) {
+//Name: New  module
+//Author: Edem Dumenu
+//Date: 03-09-2017
+//Purpose: This is creating a new controller to retrieve the types of media displayed on the left tablet
+//Arguments: "leftMedia" which is the name of the controller. $scope which is the scope object. $http which is an angularjs service
+//for reading data from server.
+//Errors/Exceptions: None
+//Modification history: This is the original version
+app.controller('leftMedia', ['$scope', '$http', function($scope, $http) {
+    var accessData = window.localStorage['leftClick'];
+    $http.get("http://34.200.170.137/abcblock/sessions/"+ accessData +"/media?left",
+        {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} })
+        .then(function (response) {
         $scope.ctObject = response.data;
         $scope.records = $scope.ctObject;
-        //$scope.message = $scope.ctObject.first_name + ' ' + $scope.ctObject.last_name;
     }, function (response)
     {
         $scope.records = "Invalid input!";
     });
+}]);
+
+//Name: New  module
+//Author: Edem Dumenu
+//Date: 03-09-2017
+//Purpose: This is creating a new controller to retrieve the left clicks the patient made during a session.
+//Arguments: "leftClicks" which is the name of the controller. $scope which is the scope object. $http which is an angularjs service
+//for reading data from server.
+//Errors/Exceptions: None
+//Modification history: This is the original version
+app.controller('leftClicks', ['$scope', '$http', function($scope, $http) {
+    var accessData = window.localStorage['patientNum'];
+    $http.get("http://34.200.170.137/abcblock/sessions/" + accessData + "/session_data?left", {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} }).then(function (response) {
+        $scope.pObject2 = response.data;
+        $scope.left = $scope.pObject2;
+        window.localStorage['leftClick'] = angular.toJson($scope.pObject2.id);
+    }, function (response)
+    {
+        $scope.left = "Invalid input!";
+    });
 
 }]);
 
-app.controller('myCtrl4', ['$scope', '$http', function($scope, $http) {
-    $http.get("http://34.200.170.137/abcblock/sessions/1/media?left", {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} }).then(function (response) {
+//Name: New  module
+//Author: Edem Dumenu
+//Date: 03-09-2017
+//Purpose: This is creating a new controller to retrieve the right clicks the patient made during a session.
+//Arguments: "rightClicks" which is the name of the controller. $scope which is the scope object. $http which is an angularjs service
+//for reading data from server.
+//Errors/Exceptions: None
+//Modification history: This is the original version
+app.controller('rightClicks', ['$scope', '$http', function($scope, $http) {
+    var accessData = window.localStorage['patientNum'];
+    $http.get("http://34.200.170.137/abcblock/sessions/"+ accessData +"/session_data?right", {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} }).then(function (response) {
+        $scope.pObject = response.data;
+        $scope.right = $scope.pObject;
+        window.localStorage['rightClick'] = angular.toJson($scope.pObject.id);
+    }, function (response)
+    {
+        $scope.right = "Invalid input!";
+    });
+}]);
+
+//Name: New  module
+//Author: Edem Dumenu
+//Date: 03-11-2017
+//Purpose: This is creating a new controller to retrieve the types of media displayed on the right tablet
+//Arguments: "rightMedia" which is the name of the controller. $scope which is the scope object. $http which is an angularjs service
+//for reading data from server.
+//Errors/Exceptions: None
+//Modification history: This is the original version
+app.controller('rightMedia', ['$scope', '$http', function($scope, $http) {
+    var accessData = window.localStorage['rightClick'];
+    $http.get("http://34.200.170.137/abcblock/sessions/"+ accessData +"/media?right", {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} }).then(function (response) {
         $scope.ctObject = response.data;
-        $scope.records1 = $scope.ctObject;
-        console.log($scope.records1);
-        //$scope.message = $scope.ctObject.first_name + ' ' + $scope.ctObject.last_name;
+        $scope.records = $scope.ctObject;
     }, function (response)
     {
-        $scope.records1 = "Invalid input!";
+        $scope.records = "Invalid input!";
     });
 }]);
 
-app.controller('myCtrl5', ['$scope', '$http', function($scope, $http) {
-    $http.get("http://34.200.170.137/abcblock/sessions/1/session_data?left", {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} }).then(function (response) {
-        $scope.ctObject2 = response.data;
-        $scope.records = $scope.ctObject2;
-        //console.log($scope.records2);
-        //$scope.message = $scope.ctObject.first_name + ' ' + $scope.ctObject.last_name;
+//Name: New  module
+//Author: Edem Dumenu
+//Date: 03-11-2017
+//Purpose: This is creating a new controller to retrieve the patient's name after selecting a patient
+//Arguments: "patientName" which is the name of the controller. $scope which is the scope object. $http which is an angularjs service
+//for reading data from server.
+//Errors/Exceptions: None
+//Modification history: This is the original version
+app.controller('patientName', ['$scope', '$http', function($scope, $http) {
+   var patientID = window.localStorage['patientNum'];
+    $http.get("http://34.200.170.137/abcblock/patients/"+ patientID, {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} }).then(function (response) {
+        $scope.pObject = response.data;
+        $scope.patient = $scope.pObject.first_name + '  ' + $scope.pObject.last_name;
     }, function (response)
     {
-        $scope.records2 = "Invalid input!";
+        $scope.patient = "Invalid input!";
     });
-
 }]);
 
-app.controller('myCtrl6', ['$scope', '$http', function($scope, $http) {
-    $http.get("http://34.200.170.137/abcblock/sessions/1/media?right", {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} }).then(function (response) {
+//Name: New  module
+//Author: Edem Dumenu
+//Date: 03-11-2017
+//Purpose: This is creating a new controller to retrieve the list of sessions a patient has taken part of.
+//Arguments: "sessionList" which is the name of the controller. $scope which is the scope object. $http which is an angularjs service
+//for reading data from server.
+//Errors/Exceptions: None
+//Modification history: This is the original version
+app.controller('sessionList', ['$scope', '$http', '$sce', function($scope, $http) {
+    var patientID = window.localStorage['patientNum'];
+    $http.get("http://34.200.170.137/abcblock/sessions/patient/"+ patientID + "?row_start=0&num_rows=5", {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} }).then(function (response) {
         $scope.ctObject = response.data;
-        $scope.records1 = $scope.ctObject;
-        console.log($scope.records1);
-        //$scope.message = $scope.ctObject.first_name + ' ' + $scope.ctObject.last_name;
+        $scope.patientLists = $scope.ctObject;
     }, function (response)
     {
-        $scope.records1 = "Invalid input!";
-    });
-}]);
-
-app.controller('myCtrl7', ['$scope', '$http', function($scope, $http) {
-    $http.get("http://34.200.170.137/abcblock/sessions/1/session_data?right", {headers : {"Authorization": "Basic ZWR1bWVudTplZHVtZW51"} }).then(function (response) {
-        $scope.ctObject2 = response.data;
-        $scope.records = $scope.ctObject2;
-        //console.log($scope.records2);
-        //$scope.message = $scope.ctObject.first_name + ' ' + $scope.ctObject.last_name;
-    }, function (response)
-    {
-        $scope.records2 = "Invalid input!";
+        $scope.patientList = "Invalid input!";
     });
 
 }]);
+
+//Name: New  module
+//Author: Edem Dumenu
+//Date: 03-12-2017
+//Purpose: This is creating a new function to set the location of a link
+//Arguments: "href" which is the name of the function.
+//Errors/Exceptions: None
+//Modification history: This is the original version
+app.directive('href', function() {
+    return {
+        compile: function(element) {
+            element.attr('target', '_self');
+        }
+    };
+});
 
 
